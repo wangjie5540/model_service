92ac157a98231378fe72dc89b20dcc8254e729e5
王同贵 <wangtonggui@digitforce.com>
2022-08-05 11:35:36 +0800

    schema的表信息添加中文展示

--- solution-application/src/main/java/com/digitforce/aip/facade/SchemaQryFacadeImpl.java
+++ solution-application/src/main/java/com/digitforce/aip/facade/SchemaQryFacadeImpl.java
@@ -11,12 +11,11 @@ import com.digitforce.framework.api.dto.Result;
 import com.google.common.collect.Lists;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-
-import javax.annotation.Resource;
 
 /**
  * schema查询实现类
@@ -32,6 +31,7 @@ public class SchemaQryFacadeImpl implements SchemaQryFacade {
     static {
         SchemaDTO schemaDTO = new SchemaDTO();
         schemaDTO.setTableName("goods_table");
+        schemaDTO.setTableCname("物品表");
         List<PropertyDesc> propertyDescList = Lists.newArrayList();
         propertyDescList.add(new PropertyDesc("category", "品类", DataTypeEnum.STRING));
         propertyDescList.add(new PropertyDesc("tags", "物品标签", DataTypeEnum.STRING));
@@ -40,6 +40,7 @@ public class SchemaQryFacadeImpl implements SchemaQryFacade {
         schemaMap.put("goods_table", schemaDTO);
         schemaDTO = new SchemaDTO();
         schemaDTO.setTableName("user_table");
+        schemaDTO.setTableCname("用户表");
         propertyDescList = Lists.newArrayList();
         propertyDescList.add(new PropertyDesc("age", "品类", DataTypeEnum.NUMERIC));
         propertyDescList.add(new PropertyDesc("last_login_time", "上次登录时间", DataTypeEnum.DATETIME));
