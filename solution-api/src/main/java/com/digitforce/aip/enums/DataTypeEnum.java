3a12e3e68a14cb136d234accc223bbab9827b8cf
王同贵 <wangtonggui@digitforce.com>
2022-08-22 15:36:56 +0800

    为demo演示准备mock数据

--- solution-api/src/main/java/com/digitforce/aip/enums/DataTypeEnum.java
+++ solution-api/src/main/java/com/digitforce/aip/enums/DataTypeEnum.java
@@ -3,6 +3,8 @@ package com.digitforce.aip.enums;
 public enum DataTypeEnum {
     STRING("字符串"),
     NUMERIC("数字"),
+    BOOL("布尔"),
+    ARRAY("数组"),
     DATETIME("日期");
     final String cname;
 
