23322cf97ce321d6af76c19763f6de055822593d
王同贵 <wangtonggui@digitforce.com>
2022-09-21 17:40:33 +0800

    筛选功能先去掉不支持的算子

--- solution-api/src/main/java/com/digitforce/aip/enums/FunctionEnum.java
+++ solution-api/src/main/java/com/digitforce/aip/enums/FunctionEnum.java
@@ -5,14 +5,14 @@ import com.fasterxml.jackson.annotation.JsonValue;
 public enum FunctionEnum {
     EQ("等于"),
     NE("不等于"),
-    GT("大于"),
-    GE("大于等于"),
-    LT("小于"),
-    LE("小于等于"),
-    IS_NULL("为空"),
-    IS_NOT_NULL("不为空"),
-    INCLUDE("包含"),
-    NOT_INCLUDE("不包含"),
+    //    GT("大于"),
+//    GE("大于等于"),
+//    LT("小于"),
+//    LE("小于等于"),
+//    IS_NULL("为空"),
+//    IS_NOT_NULL("不为空"),
+//    INCLUDE("包含"),
+//    NOT_INCLUDE("不包含"),
     BETWEEN("区间");
     final String cname;
 
