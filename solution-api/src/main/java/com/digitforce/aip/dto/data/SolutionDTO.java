c4bcb6c174626b575d308441b93a456fc8d626e8
王同贵 <wangtonggui@digitforce.com>
2022-12-19 16:59:24 +0800

    补充swagger注释，删除无用的类文件

--- solution-api/src/main/java/com/digitforce/aip/dto/data/SolutionDTO.java
+++ solution-api/src/main/java/com/digitforce/aip/dto/data/SolutionDTO.java
@@ -1,22 +1,33 @@
 package com.digitforce.aip.dto.data;
 
 import com.digitforce.aip.enums.SolutionStatusEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class SolutionDTO {
+    @Schema(description = "主键id")
     private Long id;
+    @Schema(description = "方案标题")
     private String title;
+    @Schema(description = "方案描述")
     private String description;
+    @Schema(description = "场景id")
     private Long sceneId;
+    @Schema(description = "场景名称")
     private String sceneName;
+    @Schema(description = "应用系统")
     private String system;
+    @Schema(description = "时间表达式")
     private String cron;
+    @Schema(description = "时间描述，用于前提反显")
     private String cronDesc;
+    @Schema(description = "状态")
     private SolutionStatusEnum status;
-    private LocalDateTime nextFireTime;
+    @Schema(description = "创建人")
     private String createUser;
+    @Schema(description = "更新人")
     private LocalDateTime createTime;
 }
