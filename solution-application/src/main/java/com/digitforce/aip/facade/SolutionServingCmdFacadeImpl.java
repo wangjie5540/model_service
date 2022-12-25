30e8941d512e3a97b2bb3e1497905893dc9eda0d
王同贵 <wangtonggui@digitforce.com>
2022-12-25 18:15:32 +0800

    去掉无用的代码

--- solution-application/src/main/java/com/digitforce/aip/facade/SolutionServingCmdFacadeImpl.java
+++ solution-application/src/main/java/com/digitforce/aip/facade/SolutionServingCmdFacadeImpl.java
@@ -1,21 +1,10 @@
 package com.digitforce.aip.facade;
 
-import com.digitforce.aip.config.KafkaProperties;
-import com.digitforce.aip.domain.SolutionServing;
-import com.digitforce.aip.domain.SolutionServingStatusDTO;
 import com.digitforce.aip.dto.cmd.SolutionServingAddCmd;
 import com.digitforce.aip.dto.cmd.SolutionServingDeleteCmd;
-import com.digitforce.aip.enums.StatusChangeEnum;
-import com.digitforce.aip.service.cmd.SolutionServingCmdService;
-import com.digitforce.aip.utils.ApplicationUtil;
 import com.digitforce.framework.api.dto.Result;
-import com.digitforce.framework.tool.ConvertTool;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 /**
  * 策略命令接口实现类
@@ -26,36 +15,37 @@ import javax.annotation.Resource;
  */
 @RestController
 public class SolutionServingCmdFacadeImpl implements SolutionServingCmdFacade {
-    @Resource
-    private KafkaTemplate<String, Object> kafkaTemplate;
-    @Resource
-    private SolutionServingCmdService solutionServingCmdService;
-    @Autowired(required = false)
-    private KafkaProperties kafkaProperties;
+//    @Resource
+//    private KafkaTemplate<String, Object> kafkaTemplate;
+//    @Resource
+//    private KafkaProperties kafkaProperties;
 
     @Override
     @Transactional(rollbackFor = RuntimeException.class)
     public Result add(SolutionServingAddCmd solutionServingAddCmd) {
-        SolutionServing solutionServing = ConvertTool.convert(solutionServingAddCmd, SolutionServing.class);
-        if (solutionServing.getSelection() != null) {
-            solutionServing.getSelection().forEach(tableSelection -> tableSelection.setFilterSql(ApplicationUtil.filterToSql(tableSelection.getFilter())));
-        }
-        solutionServingCmdService.save(solutionServing);
-        SolutionServingStatusDTO solutionServingStatusDTO = new SolutionServingStatusDTO();
-        solutionServingStatusDTO.setStatus(StatusChangeEnum.ADD);
-        solutionServingStatusDTO.setSolutionServing(solutionServing);
-        kafkaTemplate.send(kafkaProperties.getSolutionServingStatusTopic(), solutionServingStatusDTO);
-        return Result.success(solutionServing.getId());
+//        SolutionServing solutionServing = ConvertTool.convert(solutionServingAddCmd, SolutionServing.class);
+//        if (solutionServing.getSelection() != null) {
+//            solutionServing.getSelection().forEach(
+//                tableSelection -> tableSelection.setFilterSql(ApplicationUtil.filterToSql(tableSelection.getFilter())));
+//        }
+//        solutionServingCmdService.save(solutionServing);
+//        SolutionServingStatusDTO solutionServingStatusDTO = new SolutionServingStatusDTO();
+//        solutionServingStatusDTO.setStatus(StatusChangeEnum.ADD);
+//        solutionServingStatusDTO.setSolutionServing(solutionServing);
+//        kafkaTemplate.send(kafkaProperties.getSolutionServingStatusTopic(), solutionServingStatusDTO);
+//        return Result.success(solutionServing.getId());
+        return null;
     }
 
     @Override
     public Result delete(SolutionServingDeleteCmd solutionServingDeleteCmd) {
-        SolutionServing solutionServing = solutionServingCmdService.getById(solutionServingDeleteCmd.getServingId());
-        solutionServingCmdService.removeById(solutionServingDeleteCmd.getServingId());
-        SolutionServingStatusDTO solutionServingStatusDTO = new SolutionServingStatusDTO();
-        solutionServingStatusDTO.setStatus(StatusChangeEnum.DELETE);
-        solutionServingStatusDTO.setSolutionServing(solutionServing);
-        kafkaTemplate.send(kafkaProperties.getSolutionServingStatusTopic(), solutionServingStatusDTO);
-        return Result.success();
+//        SolutionServing solutionServing = solutionServingCmdService.getById(solutionServingDeleteCmd.getServingId());
+//        solutionServingCmdService.removeById(solutionServingDeleteCmd.getServingId());
+//        SolutionServingStatusDTO solutionServingStatusDTO = new SolutionServingStatusDTO();
+//        solutionServingStatusDTO.setStatus(StatusChangeEnum.DELETE);
+//        solutionServingStatusDTO.setSolutionServing(solutionServing);
+//        kafkaTemplate.send(kafkaProperties.getSolutionServingStatusTopic(), solutionServingStatusDTO);
+//        return Result.success();
+        return null;
     }
 }
--- solution-application/src/main/java/com/digitforce/aip/facade/SolutionServingQryFacadeImpl.java
+++ solution-application/src/main/java/com/digitforce/aip/facade/SolutionServingQryFacadeImpl.java
@@ -1,20 +1,11 @@
 package com.digitforce.aip.facade;
 
-import cn.hutool.core.convert.Convert;
-import cn.hutool.core.lang.TypeReference;
-import com.digitforce.aip.domain.SolutionServing;
 import com.digitforce.aip.dto.data.SolutionServingDTO;
 import com.digitforce.aip.dto.qry.SolutionServingGetByIdQry;
 import com.digitforce.aip.dto.qry.SolutionServingPageByQry;
-import com.digitforce.aip.service.qry.SolutionServingQryService;
-import com.digitforce.framework.api.dto.PageQuery;
 import com.digitforce.framework.api.dto.PageView;
 import com.digitforce.framework.api.dto.Result;
-import com.digitforce.framework.tool.ConvertTool;
-import com.digitforce.framework.tool.PageTool;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.Resource;
 
 /**
  * 方案服务查询接口类
@@ -25,24 +16,25 @@ import javax.annotation.Resource;
  */
 @RestController
 public class SolutionServingQryFacadeImpl implements SolutionServingQryFacade {
-    @Resource
-    private SolutionServingQryService solutionServingQryService;
+//    @Resource
+//    private SolutionServingQryService solutionServingQryService;
 
     @Override
-
     public Result<SolutionServingDTO> getById(SolutionServingGetByIdQry solutionServingGetByIdQry) {
-        SolutionServing solutionServing = solutionServingQryService.getById(solutionServingGetByIdQry.getId());
-        return Result.success(
-                solutionServing == null ? null : ConvertTool.convert(solutionServing, SolutionServingDTO.class));
+//        SolutionServing solutionServing = solutionServingQryService.getById(solutionServingGetByIdQry.getId());
+//        return Result.success(
+//            solutionServing == null ? null : ConvertTool.convert(solutionServing, SolutionServingDTO.class));
+        return null;
     }
 
     @Override
     public Result<PageView<SolutionServingDTO>> pageBy(SolutionServingPageByQry solutionServingPageByQry) {
-        PageQuery<SolutionServing> pageQuery = Convert.convert(new TypeReference<PageQuery<SolutionServing>>() {
-        }, solutionServingPageByQry);
-        PageView<SolutionServing> solutionServingPageView = solutionServingQryService.pageBy(pageQuery);
-        PageView<SolutionServingDTO> solutionServingDTOPageView = PageTool.pageView(solutionServingPageView,
-                SolutionServingDTO.class);
-        return Result.success(solutionServingDTOPageView);
+//        PageQuery<SolutionServing> pageQuery = Convert.convert(new TypeReference<PageQuery<SolutionServing>>() {
+//        }, solutionServingPageByQry);
+//        PageView<SolutionServing> solutionServingPageView = solutionServingQryService.pageBy(pageQuery);
+//        PageView<SolutionServingDTO> solutionServingDTOPageView = PageTool.pageView(solutionServingPageView,
+//            SolutionServingDTO.class);
+//        return Result.success(solutionServingDTOPageView);
+        return null;
     }
 }
