9ca037031beeca2ae8986dc38dd56e2fd65f5080
王同贵 <wangtonggui@digitforce.com>
2023-01-07 10:17:25 +0800

    解决servingInstance的pageBy返回数据缺失的问题

--- solution-api/src/main/java/com/digitforce/aip/dto/data/SolutionServingDTO.java
+++ solution-api/src/main/java/com/digitforce/aip/dto/data/SolutionServingDTO.java
@@ -1,20 +1,33 @@
 package com.digitforce.aip.dto.data;
 
 import com.digitforce.aip.enums.SceneTypeEnum;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class SolutionServingDTO {
+    @Schema(description = "方案服务id")
     private Long id;
+    @Schema(description = "标题")
     private String title;
+    @Schema(description = "场景类型")
     private SceneTypeEnum sceneType;
+    @Schema(description = "场景名称")
     private String sceneName;
+    @Schema(description = "方案标题")
     private String solutionTitle;
+    @Schema(description = "系统")
     private String system;
-    private String updateUser;
+    @Schema(description = "创建用户")
+    private String createUser;
+    @Schema(description = "方案id")
     private Long solutionId;
+    @Schema(description = "场景id")
     private Long sceneId;
+    @Schema(description = "预测时间")
+    private String cronDesc;
+    @Schema(description = "创建时间")
     private LocalDateTime createTime;
 }
