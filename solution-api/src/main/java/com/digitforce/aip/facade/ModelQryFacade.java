e23699681c8357f80c48d8ab7c358d4e57b91bae
王同贵 <wangtonggui@digitforce.com>
2023-01-10 14:00:34 +0800

    添加模型指标分页查询功能

--- solution-api/src/main/java/com/digitforce/aip/facade/ModelQryFacade.java
+++ solution-api/src/main/java/com/digitforce/aip/facade/ModelQryFacade.java
@@ -1,8 +1,10 @@
 package com.digitforce.aip.facade;
 
 import com.digitforce.aip.consts.CommonConst;
+import com.digitforce.aip.dto.data.ModelDTO;
 import com.digitforce.aip.dto.data.ModelPackageDTO;
 import com.digitforce.aip.dto.qry.ModelPackagePageByQry;
+import com.digitforce.aip.dto.qry.ModelPageByQry;
 import com.digitforce.framework.api.dto.PageView;
 import com.digitforce.framework.api.dto.Result;
 import io.swagger.v3.oas.annotations.Operation;
@@ -22,5 +24,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 public interface ModelQryFacade {
     @PostMapping("/solution/modelPackage/pageBy")
     @Operation(summary = "分页查询模型包", tags = CommonConst.SWAGGER_TAG_MODEL_QRY)
-    Result<PageView<ModelPackageDTO>> pageBy(@RequestBody ModelPackagePageByQry modelPackagePageByQry);
+    Result<PageView<ModelPackageDTO>> modelPackagePageBy(@RequestBody ModelPackagePageByQry modelPackagePageByQry);
+
+    @PostMapping("/solution/model/pageBy")
+    @Operation(summary = "分页查询模型详情", tags = CommonConst.SWAGGER_TAG_MODEL_QRY)
+    Result<PageView<ModelDTO>> pageBy(@RequestBody ModelPageByQry modelPageByQry);
 }
--- solution-application/src/main/java/com/digitforce/aip/facade/ModelQryFacadeImpl.java
+++ solution-application/src/main/java/com/digitforce/aip/facade/ModelQryFacadeImpl.java
@@ -1,9 +1,13 @@
 package com.digitforce.aip.facade;
 
+import com.digitforce.aip.dto.data.ModelDTO;
 import com.digitforce.aip.dto.data.ModelPackageDTO;
 import com.digitforce.aip.dto.qry.ModelPackagePageByQry;
+import com.digitforce.aip.dto.qry.ModelPageByQry;
+import com.digitforce.aip.entity.Model;
 import com.digitforce.aip.entity.ModelPackage;
 import com.digitforce.aip.service.IModelPackageService;
+import com.digitforce.aip.service.IModelService;
 import com.digitforce.framework.api.dto.PageView;
 import com.digitforce.framework.api.dto.Result;
 import com.digitforce.framework.tool.PageTool;
@@ -15,12 +19,22 @@ import javax.annotation.Resource;
 public class ModelQryFacadeImpl implements ModelQryFacade {
     @Resource
     private IModelPackageService modelPackageService;
+    @Resource
+    private IModelService modelService;
 
     @Override
-    public Result<PageView<ModelPackageDTO>> pageBy(ModelPackagePageByQry modelPackagePageByQry) {
+    public Result<PageView<ModelPackageDTO>> modelPackagePageBy(ModelPackagePageByQry modelPackagePageByQry) {
         PageView<ModelPackage> solutionPageView = modelPackageService.page(modelPackagePageByQry);
         PageView<ModelPackageDTO> solutionDTOPageView = PageTool.pageView(solutionPageView,
                 ModelPackageDTO.class);
+        return Result.success(solutionDTOPageView);
+    }
+
+    @Override
+    public Result<PageView<ModelDTO>> pageBy(ModelPageByQry modelPageByQry) {
+        PageView<Model> solutionPageView = modelService.page(modelPageByQry);
+        PageView<ModelDTO> solutionDTOPageView = PageTool.pageView(solutionPageView,
+                ModelDTO.class);
         return Result.success(solutionDTOPageView);
     }
 }
