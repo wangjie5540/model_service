4c8fdabcc7fbd683705f4afca61e4b0ab45a1e3a
王同贵 <wangtonggui@digitforce.com>
2023-02-03 14:07:22 +0800

    添加调用automl服务的错误提示

--- solution-application/src/main/java/com/digitforce/aip/service/impl/AutoMLServiceImpl.java
+++ solution-application/src/main/java/com/digitforce/aip/service/impl/AutoMLServiceImpl.java
@@ -5,12 +5,15 @@ import cn.hutool.http.Header;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import com.digitforce.aip.config.AutoMLProperties;
+import com.digitforce.aip.consts.SolutionErrorCode;
 import com.digitforce.aip.entity.dto.data.BestParameter;
 import com.digitforce.aip.enums.AutoMLRunStatusEnum;
 import com.digitforce.aip.service.AutoMLService;
+import com.digitforce.framework.api.exception.BizException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -18,6 +21,7 @@ import java.util.List;
 import java.util.Map;
 
 @Service
+@Slf4j
 public class AutoMLServiceImpl implements AutoMLService {
     @Resource
     private AutoMLProperties autoMLProperties;
@@ -27,13 +31,20 @@ public class AutoMLServiceImpl implements AutoMLService {
     @Override
     @SneakyThrows
     public String createTask(String templateParams) {
+        log.info("创建自动机器学习任务，请求参数：{}", templateParams);
         HttpRequest request = HttpRequest.post(StrUtil.format("{}/create-task", autoMLProperties.getBaseUrl()))
                 .header(Header.CONTENT_TYPE, "application/json")
                 .body(templateParams);
         String body = request.execute().body();
-        Map<String, Object> res = objectMapper.readValue(body, new TypeReference<Map<String, Object>>() {
-        });
-        return res.get("run_id").toString();
+        log.info("创建自动机器学习任务，返回结果：{}", body);
+        try {
+            Map<String, Object> res = objectMapper.readValue(body, new TypeReference<Map<String, Object>>() {
+            });
+            return res.get("run_id").toString();
+        } catch (Exception e) {
+            log.error("创建自动机器学习任务失败", e);
+            throw BizException.of(SolutionErrorCode.AUTO_ML_CREATE_TASK_ERROR);
+        }
     }
 
     @Override
