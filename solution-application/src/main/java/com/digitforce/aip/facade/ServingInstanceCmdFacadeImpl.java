f18064d9d7ef5b413bdcb2622a27cac1fa3220ca
王同贵 <wangtonggui@digitforce.com>
2023-03-20 14:28:07 +0800

    创建服务实例后同步创建表分区

--- solution-application/src/main/java/com/digitforce/aip/facade/ServingInstanceCmdFacadeImpl.java
+++ solution-application/src/main/java/com/digitforce/aip/facade/ServingInstanceCmdFacadeImpl.java
@@ -4,8 +4,10 @@ import com.digitforce.aip.dto.cmd.ServingInstanceAddCmd;
 import com.digitforce.aip.dto.data.ServingInstanceDTO;
 import com.digitforce.aip.entity.ServingInstance;
 import com.digitforce.aip.entity.SolutionServing;
+import com.digitforce.aip.mapper.StarrocksDDLMapper;
 import com.digitforce.aip.service.IServingInstanceService;
 import com.digitforce.aip.service.ISolutionServingService;
+import com.digitforce.aip.utils.OlapHelper;
 import com.digitforce.framework.api.dto.Result;
 import com.digitforce.framework.tool.ConvertTool;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +20,16 @@ public class ServingInstanceCmdFacadeImpl implements ServingInstanceCmdFacade {
     private ISolutionServingService solutionServingService;
     @Resource
     private IServingInstanceService servingInstanceService;
+    @Resource
+    private StarrocksDDLMapper starrocksDDLMapper;
 
     @Override
     public Result<ServingInstanceDTO> add(ServingInstanceAddCmd servingInstanceAddCmd) {
         SolutionServing solutionServing = solutionServingService.getById(servingInstanceAddCmd.getServingId());
         ServingInstance servingInstance = servingInstanceService.createAndRun(solutionServing);
         ServingInstanceDTO servingInstanceDTO = ConvertTool.convert(servingInstance, ServingInstanceDTO.class);
+        starrocksDDLMapper.createPartition(OlapHelper.getScoreTableName(solutionServing.getSolutionId()),
+                servingInstance.getId(), servingInstance.getId() + 1);
         return Result.success(servingInstanceDTO);
     }
 }
