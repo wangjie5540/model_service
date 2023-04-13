fc0f5ac1f14f4659b77937553373b632d1e8c53e
王同贵 <wangtonggui@digitforce.com>
2023-04-13 17:10:42 +0800

    添加获取预测结果明细接口

--- solution-application/src/main/java/com/digitforce/aip/facade/ServingInstanceQryFacadeImpl.java
+++ solution-application/src/main/java/com/digitforce/aip/facade/ServingInstanceQryFacadeImpl.java
@@ -1,20 +1,22 @@
 package com.digitforce.aip.facade;
 
+import com.digitforce.aip.dto.data.PredictDetailDTO;
 import com.digitforce.aip.dto.data.PredictResultDTO;
 import com.digitforce.aip.dto.data.ServingInstanceDTO;
 import com.digitforce.aip.dto.qry.GetPredictResultQry;
+import com.digitforce.aip.dto.qry.PredictDetailPageByQry;
 import com.digitforce.aip.dto.qry.ServingInstanceGetByIdQry;
 import com.digitforce.aip.dto.qry.ServingInstancePageByQry;
+import com.digitforce.aip.entity.PredictDetail;
 import com.digitforce.aip.entity.ServingInstance;
 import com.digitforce.aip.enums.ScoreRangeType;
-import com.digitforce.aip.mapper.OlapMapper;
+import com.digitforce.aip.mapper.PredictResultMapper;
 import com.digitforce.aip.service.IServingInstanceService;
 import com.digitforce.aip.utils.OlapHelper;
 import com.digitforce.framework.api.dto.PageView;
 import com.digitforce.framework.api.dto.Result;
 import com.digitforce.framework.tool.ConvertTool;
 import com.digitforce.framework.tool.PageTool;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import lombok.SneakyThrows;
@@ -39,9 +41,7 @@ public class ServingInstanceQryFacadeImpl implements ServingInstanceQryFacade {
     @Resource
     private IServingInstanceService servingInstanceService;
     @Resource
-    private OlapMapper olapMapper;
-    @Resource
-    private ObjectMapper objectMapper;
+    private PredictResultMapper predictResultMapper;
 
     @Override
     public Result<PageView<ServingInstanceDTO>> pageBy(ServingInstancePageByQry servingInstancePageByQry) {
@@ -59,27 +59,27 @@ public class ServingInstanceQryFacadeImpl implements ServingInstanceQryFacade {
 
     @Override
     @SneakyThrows
-    public Result<PredictResultDTO> getPredictResult(GetPredictResultQry getPredictResultQry) {
+    public Result<PredictResultDTO> getPredictStatistics(GetPredictResultQry getPredictResultQry) {
         ServingInstance servingInstance = servingInstanceService.getById(getPredictResultQry.getInstanceId());
-//        String tableName = OlapHelper.getScoreTableName(servingInstance.getSolutionId());
-        String tableName = OlapHelper.getScoreTableName(251L);
+        String tableName = OlapHelper.getScoreTableName(servingInstance.getSolutionId());
+//        String tableName = OlapHelper.getScoreTableName(251L);
         Map<String, Object> map;
         switch (getPredictResultQry.getScoreRangeType()) {
             case ALL:
-                map = olapMapper.all(tableName);
+                map = predictResultMapper.all(tableName, servingInstance.getId());
                 break;
             case TOP_N:
                 Long n = Long.parseLong(getPredictResultQry.getValues().get(0).toString());
-                map = olapMapper.topN(tableName, n);
+                map = predictResultMapper.topN(tableName, servingInstance.getId(), n);
                 break;
             case TOP_PERCENT:
                 Double percent = Double.parseDouble(getPredictResultQry.getValues().get(0).toString());
-                map = olapMapper.topPercent(tableName, percent);
+                map = predictResultMapper.topPercent(tableName, servingInstance.getId(), percent);
                 break;
             case SCORE_RANGE:
                 Double minScore = Double.parseDouble(getPredictResultQry.getValues().get(0).toString());
                 Double maxScore = Double.parseDouble(getPredictResultQry.getValues().get(1).toString());
-                map = olapMapper.targetScore(tableName, minScore, maxScore);
+                map = predictResultMapper.targetScore(tableName, servingInstance.getId(), minScore, maxScore);
                 break;
             default:
                 throw new RuntimeException("不支持的类型");
@@ -91,15 +91,31 @@ public class ServingInstanceQryFacadeImpl implements ServingInstanceQryFacade {
         PredictResultDTO.ScoreRange scoreRange = new PredictResultDTO.ScoreRange();
         scoreRange.setMaxScore(Double.parseDouble(map.get("max_score").toString()));
         scoreRange.setMinScore(Double.parseDouble(map.get("min_score").toString()));
-        feedInterval(predictResultDTO, getPredictResultQry.getScoreRangeType(), tableName,
+        feedInterval(predictResultDTO, getPredictResultQry.getScoreRangeType(), tableName, servingInstance.getId(),
                 getPredictResultQry.getValues());
         predictResultDTO.setScoreRange(scoreRange);
         return Result.success(predictResultDTO);
     }
 
+    @Override
+    public Result<PageView<PredictDetailDTO>> pageByPredictDetail(PredictDetailPageByQry predictResultPageByQry) {
+        ServingInstance servingInstance = servingInstanceService.getById(predictResultPageByQry.getInstanceId());
+        PageView<PredictDetailDTO> pageView = new PageView<>();
+        pageView.setCount(Integer.parseInt(predictResultPageByQry.getTotal().toString()));
+        String tableName = OlapHelper.getScoreTableName(servingInstance.getSolutionId());
+//        String tableName = OlapHelper.getScoreTableName(251L);
+        List<PredictDetail> predictDetailList = predictResultMapper.getPredictDetailList(tableName,
+                predictResultPageByQry.getInstanceId(),
+                predictResultPageByQry.getMinScore(), predictResultPageByQry.getMaxScore(),
+                predictResultPageByQry.getTotal());
+        List<PredictDetailDTO> predictDetailDTOList = ConvertTool.convert(predictDetailList, PredictDetailDTO.class);
+        pageView.setList(predictDetailDTOList);
+        return Result.success(pageView);
+    }
+
     private void feedInterval(PredictResultDTO predictResultDTO, ScoreRangeType scoreRangeType, String tableName,
-                              List<Object> values) {
-        List<Map<String, Object>> baseRange = olapMapper.getBaseRange(tableName);
+                              Long instanceId, List<Object> values) {
+        List<Map<String, Object>> baseRange = predictResultMapper.getBaseRange(tableName, instanceId);
         predictResultDTO.setBaseIntervals(baseRange.stream().map(item -> {
             PredictResultDTO.Interval interval = new PredictResultDTO.Interval();
             interval.setCname(item.get("score_range").toString());
@@ -128,7 +144,8 @@ public class ServingInstanceQryFacadeImpl implements ServingInstanceQryFacade {
                 predictResultDTO.setTargetIntervals(targetIntervals);
                 break;
             case SCORE_RANGE:
-                List<Map<String, Object>> rangeDistribution = olapMapper.getTargetScoreDistribution(tableName,
+                List<Map<String, Object>> rangeDistribution = predictResultMapper.getTargetScoreDistribution(tableName,
+                                instanceId,
                                 Double.parseDouble(values.get(0).toString()),
                                 Double.parseDouble(values.get(1).toString()))
                         .stream().map(item -> {
---
+++ solution-start/src/test/java/com/digitforce/aip/mapper/PredictDetailMapperTest.java
@@ -0,0 +1,49 @@
+package com.digitforce.aip.mapper;
+
+
+import com.digitforce.aip.test.BaseTest;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+public class PredictDetailMapperTest extends BaseTest {
+    @Resource
+    private PredictResultMapper predictResultMapper;
+
+    @Test
+    public void topN() {
+        Map<String, Object> total = predictResultMapper.topN("aip.score_251", 1645666375508107265L, 20L);
+        System.out.println(total);
+    }
+
+    @Test
+    public void topPercent() {
+        Map<String, Object> total = predictResultMapper.topPercent("aip.score_241", 1645666375508107265L, 0.2);
+        System.out.println(total);
+    }
+
+    @Test
+    public void targetScore() {
+        Map<String, Object> total = predictResultMapper.targetScore("aip.score_241", 1645666375508107265L, 0.2, 0.8);
+        System.out.println(total);
+    }
+
+    @Test
+    public void scoreRange() {
+        Object total = predictResultMapper.getBaseRange("aip.score_241", 1645666375508107265L);
+        System.out.println(total);
+    }
+
+    @Test
+    public void targetScoreDistribution() {
+        Object total = predictResultMapper.getTargetScoreDistribution("aip.score_251", 1645666375508107265L, 0.15, 0.8);
+        System.out.println(total);
+    }
+
+    @Test
+    public void getPredictDetailList() {
+        Object total = predictResultMapper.getPredictDetailList("aip.score_251", 1645666375508107265L, 0.15, 0.8, 10L);
+        System.out.println(total);
+    }
+}
