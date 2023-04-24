82401780bcf689131a764fc1deda834c868b8764
王同贵 <wangtonggui@digitforce.com>
2023-04-24 14:54:08 +0800

    添加选取目标用户下载功能

--- solution-application/src/main/java/com/digitforce/aip/service/component/DownloadService.java
+++ solution-application/src/main/java/com/digitforce/aip/service/component/DownloadService.java
@@ -1,44 +1,55 @@
 package com.digitforce.aip.service.component;
 
-import com.digitforce.aip.entity.UserScore;
-import com.digitforce.aip.mapper.PredictResultMapper;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.dynamic.datasource.DynamicRoutingDataSource;
 import com.digitforce.aip.utils.OlapHelper;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.UncheckedIOException;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Stream;
 
 @Service
+@Slf4j
 public class DownloadService {
     @Resource
-    private PredictResultMapper predictResultMapper;
+    private DataSource dataSource;
 
     @SneakyThrows
     public void downloadResult(OutputStream outputStream, Long solutionId, Long instanceId, Double start, Double end,
                                Long total) {
+        DynamicRoutingDataSource dynamicRoutingDataSource = (DynamicRoutingDataSource) dataSource;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dynamicRoutingDataSource.getDataSource("olap"));
+        String sql = StrUtil.format("select user_id, score from {} where instance_id = {} and score between {} and {}" +
+                        " order by score desc limit {}",
+                OlapHelper.getScoreTableName(solutionId),
+                instanceId.toString(),
+                start.toString(),
+                end.toString(),
+                total.toString()
+        );
+        log.info("query sql: {}", sql);
         try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, StandardCharsets.UTF_8))) {
             // Write users data
-            String tableName = OlapHelper.getScoreTableName(solutionId);
-            try (Stream<UserScore> userStream = predictResultMapper
-                    .getPredictDetailList(tableName, instanceId, start, end, total)) {
-                userStream.forEach(userScore -> {
-                    try {
-                        writer.write(userScore.getUserId() + "," + userScore.getScore());
-                        writer.newLine();
-                    } catch (IOException e) {
-                        throw new UncheckedIOException(e);
-                    }
-                });
-            } catch (UncheckedIOException e) {
-                throw e.getCause();
-            }
+            jdbcTemplate.query(sql, resultSet -> {
+                try {
+                    String userId = resultSet.getString("user_id");
+                    double score = resultSet.getDouble("score");
+
+                    writer.write(userId + "," + score);
+                    writer.newLine();
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            });
         }
     }
 }
